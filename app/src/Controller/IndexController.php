a7b44a18e72c9af97e19f8662343b51bc57cdf9e
Tanzar <spakowski19@gmail.com>
2023-05-15 14:03:28 +0200

    Initial Commit

---
+++ app/src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController {
+
+    #[Route('/', name: 'index')]
+    public function index() {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/date', name: 'date')]
+    public function date() {
+        $date = new DateTime();
+        return new Response($date->format('e: Y-m-d h:i:s'));
+    }
+}
